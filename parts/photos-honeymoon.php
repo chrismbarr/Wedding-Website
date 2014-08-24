252d36e417174aa019334e2de03994118daa6862
Chris Barr <chris.m.barr@gmail.com>
2014-08-23 23:22:32 -0400

    Adding honeymoon photos

--- parts/photos-honeymoon.php
+++ parts/photos-honeymoon.php
@@ -1,1 +1,16 @@
 <h2 class="section-title">Honeymoon Photos</h2>
+
+<div class="row">
+	<?
+	for ($i=1; $i <= 50; $i++) {
+		$num = $i < 10 ? "0".$i : $i;
+	?>
+		<div class="col-xs-3 col-md-2">
+			<a href="images/honeymoon-<? echo $num; ?>.jpg" class="thumbnail" data-lightbox="honeymoon">
+				<img class="img-rounded" src="images/honeymoon-<? echo $num; ?>-thumb.jpg" alt="">
+			</a>
+		</div>
+	<? } ?>
+</div>
+
+<p>photos by <a href="http://chris-barr.com">Chris Barr</a></h3>
