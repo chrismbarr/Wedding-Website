fd8d992de0ad52f034ed9cb58c77b00cb778d5c6
Chris Barr <chris.m.barr@gmail.com>
2014-08-24 00:13:52 -0400

    Adding Facebook wedding photos

--- parts/photos-wedding.php
+++ parts/photos-wedding.php
@@ -1,3 +1,20 @@
 <h2 class="section-title">Wedding Photos</h2>
 
-<h3 class="text-center">Coming Soon!</h3>
+<div class="row">
+	<?
+	for ($i=1; $i <= 15; $i++) {
+		$num = $i < 10 ? "0".$i : $i;
+	?>
+		<div class="col-xs-3 col-md-2">
+			<a href="images/wedding-fb-<? echo $num; ?>.jpg" class="thumbnail" data-lightbox="wedding-fb">
+				<img class="img-rounded" src="images/wedding-fb-<? echo $num; ?>-thumb.jpg" alt="">
+			</a>
+		</div>
+	<? } ?>
+</div>
+
+<p>photos from friends on facebook</p>
+
+
+<hr />
+<h3 class="text-center">Professional Photos Coming Soon!</h3>
