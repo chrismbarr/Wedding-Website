d6835e2ff5df531870bbe01ef949608196c39440
Chris Barr <chris.m.barr@gmail.com>
2014-08-23 20:15:35 -0400

    Adding engagement photos

--- parts/photos-engagement.php
+++ parts/photos-engagement.php
@@ -1,1 +1,16 @@
 <h2 class="section-title">Engagement Photos</h2>
+
+<div class="row">
+	<?
+	for ($i=1; $i <= 15; $i++) {
+		$num = $i < 10 ? "0".$i : $i;
+	?>
+		<div class="col-xs-3 col-md-2">
+			<a href="images/engagement-<? echo $num; ?>.jpg" class="thumbnail" data-lightbox="engagement">
+				<img class="img-rounded" src="images/engagement-<? echo $num; ?>-thumb.jpg" alt="">
+			</a>
+		</div>
+	<? } ?>
+</div>
+
+<p>photos by <a href="http://swakphotography.com">SWAK Photography</a></h3>
--- parts/quote.php
+++ parts/quote.php
@@ -6,7 +6,5 @@
 <div id="credits">
 	<span class="credits">website by <a href="http://chris-barr.com">Chris Barr</a></span>
 	<span class="credits-divider">|</span>
-	<span class="credits">photos by <a href="http://swakphotography.com">SWAK Photography</a></span>
-	<span class="credits-divider">|</span>
 	<span class="credits">Get the code for this site <a href="https://github.com/chrismbarr/Wedding-Website">on GitHub</a>!</span>
 </div>
